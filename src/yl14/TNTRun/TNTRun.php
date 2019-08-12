bd3db1c5544f6e33274da71811d2c6316410a395
KenMizz <mizzken16@gmail.com>
2019-08-12 23:40:16 +0800

    完成一些基础向

--- src/yl14/TNTRun/TNTRun.php
+++ src/yl14/TNTRun/TNTRun.php
@@ -12,22 +12,51 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\{
     TextFormat as TF, Config
 };
+use pocketmine\command\{
+    CommandSender, Command
+};
 
 use yl13\GameCoreAPI\GameCoreAPI;
 
 class TNTRun extends PluginBase {
 
+    /** * @var string*/
     private $gcid;
 
+    /** *@var TNTRun*/
     private static $instance;
 
-    public function onEnable() {
+    /** *@var Array*/
+    private $Sessions = [];
+
+    public function onEnable() : void{
         $this->gcid = GameCoreAPI::getInstance()->api->getGameCoreAPI()->registerGame("TNT跑酷", "游乐14");
         $this->getLogger()->notice(TF::GREEN . "TNT跑酷插件已启动！作者:游乐14");
         $this->getLogger()->notice(TF::YELLOW . "当前版本: v1.0.0_TEST");
     }
 
-    public function onLoad() {
+    public function onLoad() : void{
         self::$instance = $this;
     }
+
+    public function onDisable() : void{
+        $this->getLogger()->warning("TNT跑酷已关闭！");
+    }
+
+    static public function getInstance() :TNTRun {
+        return self::$instance;
+    }
+
+    public function findSession(Array $filter = []) : ?int {
+        if(isset($filter['map'])) {
+
+        }
+        //TODO
+    }
+
+    private function getSession(int $sessionid) : TNTRunSession {
+        //TODO
+    }
+
+    private function createSession()
 }
---
+++ src/yl14/TNTRun/TNTRunSession.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace yl14\TNTRun;
+
+use pocketmine\Player;
+
+class TNTRunSession {
+
+    /** *@var int*/
+    private $sessionid = 0;
+
+    /** *@var pocketmine\Player[]*/
+    private $players = [];
+
+    /** *@var Array*/
+    private $settings = [];
+}
